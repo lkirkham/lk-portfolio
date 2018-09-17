bf140e9cae2f425a12cab1bd9a2470fafcdbfbc7
Laura Kirkham <kirkham.laura@hotmail.com>
2018-09-17 15:45:27 -0400

    added animations and header

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,18 +1,40 @@
 import Typed from "typed.js";
+import AOS from "aos";
 
 
+//typed JS
 var introHeading = new Typed("#introHeading", {
   strings: [
-    // "I am a",
-    // "I am a front-end developer",
-    // "I am a web designer",
-    // "I am a digital experience manager"
     "a front-end developer.",
     "a web designer.",
-    "a digital product owner.",
+    "a digital product manager.",
   ],
   typeSpeed: 10,
   backSpeed: 10,
-  smartBackspace: true, // this is a default
+  smartBackspace: true,
   loop: true
 });
+
+
+//animate on scroll library
+AOS.init();
+
+// // You can also pass an optional settings object
+// // below listed default settings
+// AOS.init({
+//   // Global settings
+//   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//   initClassName: 'aos-init', // class applied after initialization
+//   animatedClassName: 'aos-animate', // class applied on animation
+//   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+
+//   // Settings that can be overriden on per-element basis, by `data-aos-*` attributes:
+//   offset: 120, // offset (in px) from the original trigger point
+//   delay: 0, // values from 0 to 3000, with step 50ms
+//   duration: 400, // values from 0 to 3000, with step 50ms
+//   easing: 'ease', // default easing for AOS animations
+//   once: false, // whether animation should happen only once - while scrolling down
+//   mirror: false, // whether elements should animate out while scrolling past them
+//   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+// });
